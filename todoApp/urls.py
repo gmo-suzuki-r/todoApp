439383e6f6bb68ad904b74124efb656e617d3cac
Suzuki Ren <suzuki-r@gmo-connect.jp>
2023-01-05 17:36:19 +0900

    no message

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -3,5 +3,5 @@ from .views import todoList, todoDetail
 
 urlpatterns = [
     path('list/', todoList.as_view()),
-    path('detail/', todoDetail.as_view()),
+    path('detail/<int:pk>', todoDetail.as_view()),
 ]
