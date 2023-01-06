09b12d92e410a00541ae7a2790f62394e5c73764
Suzuki Ren <suzuki-r@gmo-connect.jp>
2023-01-06 09:24:11 +0900

    no message

--- todoApp/models.py
+++ todoApp/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+PRIORITY = (('danger', 'high'), ('info', 'nomal',), ('success', 'low'))
+
 
 class todoModel(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField()
+    priority = models.CharField(
+        max_length=50,
+        choices=PRIORITY
+    )
+    duedate = models.DateField()
 
     def __str__(self):
         return self.title
